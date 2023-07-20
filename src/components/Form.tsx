94eff083bb854bab30f8ca11f0a14ab546a846f3
tauseef-creator <i211236@nu.edu.pk>
2023-07-20 12:51:09 +0500

    Validate through zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,39 +1,66 @@
 import React, { FormEvent, useRef } from "react";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-    name: string;
-    age: string;
-}
+const schema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "Age must be at least 18 years old" }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
-    const { register, handleSubmit, formState: {errors}}= useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
-//   const Person = {
-//     name: "",
-//     age: "",
-//   };
-//   const handleSubmit = (event: FormEvent) => {
-//     event.preventDefault();
-//     if (nameRef.current) Person.name = nameRef.current.value;
-//     if (ageRef.current) Person.age = ageRef.current.value;
-//     console.log(Person);
-//   };
+  //   const Person = {
+  //     name: "",
+  //     age: "",
+  //   };
+  //   const handleSubmit = (event: FormEvent) => {
+  //     event.preventDefault();
+  //     if (nameRef.current) Person.name = nameRef.current.value;
+  //     if (ageRef.current) Person.age = ageRef.current.value;
+  //     console.log(Person);
+  //   };
   return (
-    <form onSubmit={handleSubmit(data => console.log(data)) }>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input {...register('name', {required: true, minLength: 3})} id="name" type="text" className="form-control" />
-        { errors.name?.type === 'required' && <small className="text-danger">Name is a required field</small>}
-        { errors.name?.type === 'minLength' && <small className="text-danger">Name must be atleast 3 characters long.</small>}
+        <input
+          {...register("name")}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name && (
+          <small className="text-danger">{errors.name.message}</small>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input {...register('age')} id="age" type="number" className="form-control" />
+        <input
+          {...register("age", { valueAsNumber: true })}
+          id="age"
+          type="number"
+          className="form-control"
+        />
+
+        {errors.age && (
+          <small className="text-danger">{errors.age.message}</small>
+        )}
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
